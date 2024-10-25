617d434a614aba197f40be4468754f16efd71bb7
Yehor Kryvoi <126288081+TurboD1reX@users.noreply.github.com>
2024-10-25 23:56:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ document.getElementById('search-input').addEventListener('input', function() {
     });
 });
 
+const ingredients = document.querySelectorAll('.ingredients');
+const ingredient = document.querySelectorAll('.ingredient');
+const cocktails = document.querySelectorAll('.cocktail');
+
+let selectedIngredients = new Set();
+
+
+function filterCocktails() {
+    cocktails.forEach(cocktail => {
+        const cocktailIngredients = [...cocktail.querySelectorAll('.ingredient')].map(ing => ing.getAttribute('data-name'));
+        const hasAllSelectedIngredients = [...selectedIngredients].every(ing => cocktailIngredients.includes(ing));
+        
+        cocktail.style.display = hasAllSelectedIngredients ? 'block' : 'none';
+    });
+}
+
+
+ingredients.forEach(ingredient => {
+    ingredient.addEventListener('click', () => {
+        const ingredientName = ingredient.getAttribute('data-name');
+
+
+        if (selectedIngredients.has(ingredientName)) {
+            selectedIngredients.delete(ingredientName);
+            ingredient.classList.remove('selected');
+        } else {
+            selectedIngredients.add(ingredientName);
+            ingredient.classList.add('selected');
+        }
+
+        filterCocktails();
+    });
+});
